ce71b65e9cce9948dcccda1244f0bd6befcb7b63
Oktay Ibis <oktayibis@gmail.com>
2020-05-19 19:25:29 +0300

    sign-in page and componenets created

---
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./sign-in-and-sign-out.styles.scss"
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInAndSignOutPage = () => (
+    <div>
+    <SignIn />
+    </div>
+
+);
+
+
+export default SignInAndSignOutPage;
