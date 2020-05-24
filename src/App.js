7406eaf92d4193f65e6e3104f2c6a9b83d4ae548
Oktay Ibis <oktayibis@gmail.com>
2020-05-25 00:16:21 +0300

    implement fireabase utils, inculede sign in with google and sign up with email

--- src/App.js
+++ src/App.js
@@ -5,28 +5,37 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignOutPage from "./pages/sign-in-and-sign-out/sign-in-and-sign-out.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
+	const [currentUser, setCurrentUser] = useState(null);
 
-
-useEffect(() => {
-auth.onAuthStateChanged(user=> {
-    setCurrentUser(user);
-    console.log("Login: ", currentUser )
-  })
-}, [currentUser])
-  return (
-    <div>
-      <Header currentUser={currentUser} />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignOutPage} />
-      </Switch>
-    </div>
-  );
+	useEffect(() => {
+		auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth); // First check is user exist, if no create user in database and return userRef number
+				userRef.onSnapshot((snapShot) => {
+					setCurrentUser({
+						id: snapShot.id, // Call user id with snapShot object
+						...snapShot.data(), // Call snapShot data with .data() method
+					});
+				});
+			} else {
+				setCurrentUser(userAuth);
+			}
+		});
+	}, []);
+	console.log("CurrentUSer:", currentUser);
+	return (
+		<div>
+			<Header currentUser={currentUser} />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/shop" component={ShopPage} />
+				<Route path="/signin" component={SignInAndSignOutPage} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,38 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCiWmdnUkqQ4wcjNsY-pKdil4tsCFQUZyk",
-  authDomain: "ztm-commerce-db.firebaseapp.com",
-  databaseURL: "https://ztm-commerce-db.firebaseio.com",
-  projectId: "ztm-commerce-db",
-  storageBucket: "ztm-commerce-db.appspot.com",
-  messagingSenderId: "172803513185",
-  appId: "1:172803513185:web:dbdbefc1caa53251b14320",
-  measurementId: "G-3PZWP3VNME",
+	apiKey: "AIzaSyCiWmdnUkqQ4wcjNsY-pKdil4tsCFQUZyk",
+	authDomain: "ztm-commerce-db.firebaseapp.com",
+	databaseURL: "https://ztm-commerce-db.firebaseio.com",
+	projectId: "ztm-commerce-db",
+	storageBucket: "ztm-commerce-db.appspot.com",
+	messagingSenderId: "172803513185",
+	appId: "1:172803513185:web:dbdbefc1caa53251b14320",
+	measurementId: "G-3PZWP3VNME",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+	
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData,
+			});
+		} catch (err) {
+			console.log("error: ", err.message);
+    }
+    
+	}
+  return userRef;
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -18,7 +42,7 @@ firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-// Google sing-in
+// Google sign-in
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
